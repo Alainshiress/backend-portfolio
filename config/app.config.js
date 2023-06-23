776ef3e279d83647ca7df72e1553003fc9894f1e
Alainshiress <alainshiress64@gmail.com>
2023-06-23 02:30:09 -1100

    message

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+   DB: 'mongodb+srv://alainshiress64:alain@backend.2qpltuz.mongodb.net/Backend?retryWrites=true&w=majority'
+    
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
